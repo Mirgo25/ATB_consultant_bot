68b1ac3c35c275f6f916dd80f553f1c4f090ce5d
Mirgo25 <mirgo2513@gmail.com>
2024-01-06 21:57:28 +0200

    connect telegraf and sessions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from './env';
+import { TelegrafConfigService } from './config/telegraf.config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: getEnvPath(),
+    }),
+    TelegrafModule.forRootAsync({
+      useClass: TelegrafConfigService,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TelegrafConfigService],
 })
 export class AppModule {}
---
+++ src/config/telegraf.config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TelegrafModuleOptions, TelegrafOptionsFactory } from 'nestjs-telegraf';
+import LocalSession from 'telegraf-session-local';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
+
+@Injectable()
+export class TelegrafConfigService implements TelegrafOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTelegrafOptions(): TelegrafModuleOptions {
+    return {
+      token: this.configService.getOrThrow('BOT_TOKEN'),
+      middlewares: [sessions.middleware()],
+    };
+  }
+}
