fbc23be73aea00c9316f451291d4de842a927a21
Mirgo25 <mirgo2513@gmail.com>
2024-02-11 12:57:00 +0200

    added action for parse tiktok url

--- src/app.update.ts
+++ src/app.update.ts
@@ -1,6 +1,19 @@
 import { AppService } from './app.service';
 import { Context, Telegraf } from 'telegraf';
-import { InjectBot, Start, Update } from 'nestjs-telegraf';
+import {
+  InjectBot,
+  Start,
+  TextLink,
+  Update,
+  TextMention,
+  Action,
+  On,
+  Url,
+  Message,
+  Ctx,
+} from 'nestjs-telegraf';
+import axios, { AxiosResponse, RawAxiosRequestHeaders } from 'axios';
+import { NotFoundException } from '@nestjs/common';
 
 @Update()
 export class AppUpdate {
@@ -12,5 +25,28 @@ export class AppUpdate {
   @Start()
   async getHello(ctx: Context) {
     await ctx.reply('Hi, Friend!');
+  }
+  // https://vm.tiktok.com/ZIJnS8CgJ
+  // https://www.tiktok.com/@mcgregor_millionaire/video/7306885065437482273?_r=1&_t=8icn5CUJdSq
+  @Url(new RegExp('https://vm.tiktok.com/'))
+  async getTikTokLink(@Message('text') link: string, @Ctx() ctx: Context) {
+    console.log({ link });
+    // const headers: RawAxiosRequestHeaders = {
+    //   'User-Agent':
+    //     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    // };
+    const { request }: { request: { path: string } } = await axios.get(link);
+    if (!request) {
+      await ctx.reply('Something wrong with link you have sent');
+      throw new NotFoundException('Request not found');
+    }
+
+    const userName = request.path.match(/^\/(@\w+)/)[1];
+    const videoId = request.path.match(/\/video\/(\d+)/)[1];
+
+    console.log({userName})
+    console.log({videoId})
+
+    await ctx.reply('Good!');
   }
 }
